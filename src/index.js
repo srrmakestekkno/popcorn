e2ff9c2a4affe816ec1f9f142b22f18f662d395e
stian røgeberg <srr007@hotmail.com>
2024-09-06 12:06:20 +0200

    StarRating component more flexible with props

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "OK", "Good", "Amazing"]}
+    />
+    <StarRating
+      size={24}
+      maxRating={12}
+      color="#987865"
+      className="test"
+      defaultRating={3}
+    />
   </React.StrictMode>
 );
